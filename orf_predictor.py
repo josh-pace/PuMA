bacd654ad6c8d93389f0fd9eccedf1970165608d
Ken Youens-Clark <kyclark@gmail.com>
2018-03-01 10:28:42 -0700

    now can be a shell script

--- orf_predictor.py
+++ orf_predictor.py
@@ -8,9 +8,26 @@ import sys
 from Bio import SeqIO
 
 # --------------------------------------------------
+def get_args():
+    """get args"""
+    parser = argparse.ArgumentParser(description='Argparse Python script',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file', metavar='FILE', help='Input sequence file')
+    parser.add_argument('-f', '--format', help='Input file format',
+                        metavar='STR', type=str, default='fasta')
+    parser.add_argument('-o', '--outfile', help='Output file',
+                        metavar='STR', type=str, default='orfs.fa')
+    parser.add_argument('-m', '--min', help='Minimum length',
+                        metavar='INT', type=int, default=50)
+    parser.add_argument('-t', '--trans', help='Translation table',
+                        metavar='INT', type=int, default=1)
+    return parser.parse_args()
+
+# --------------------------------------------------
 def find_orfs(seq, trans_table, min_prot_len):
     """Find all ORFs in the sequence"""
-    orfs_re = re.compile(r'(M.+?[*])')
+    orfs_re = re.compile(r'(M[^*]+)')
     orfs = []
 
     for frame in range(3):
@@ -22,28 +39,13 @@ def find_orfs(seq, trans_table, min_prot_len):
         trans = str(seq[frame:frame_end].translate(trans_table))
 
         for match in orfs_re.finditer(trans):
-            aa_seq = str(match.groups(0))
+            aa_seq = match.group(0)
             if len(aa_seq) >= min_prot_len:
-                orfs.append({'seq': aa_seq, 
+                orfs.append({'seq': aa_seq,
                              'start': frame + (match.start() * 3),
                              'stop': frame + (match.end() * 3) - 1})
 
     return orfs
-
-# --------------------------------------------------
-def get_args():
-    """get args"""
-    parser = argparse.ArgumentParser(description='Argparse Python script',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('file', metavar='FILE', help='Input sequence file')
-    parser.add_argument('-f', '--format', help='Input file format',
-                        metavar='STR', type=str, default='fasta')
-    parser.add_argument('-m', '--min', help='Minimum length',
-                        metavar='INT', type=int, default=50)
-    parser.add_argument('-t', '--trans', help='Translation table',
-                        metavar='INT', type=int, default=1)
-    return parser.parse_args()
 
 # --------------------------------------------------
 def main():
@@ -53,19 +55,29 @@ def main():
     file_format = args.format
     min_len = args.min
     trans_table = args.trans
+    out_file = args.outfile
 
     if not os.path.isfile(infile):
         print('"{}" is not a file'.format(infile))
         sys.exit(1)
 
-    for i, rec in enumerate(SeqIO.parse("{}".format(infile), file_format)):
-        print('>{:3}: {}'.format(i+1, rec.id))
-        orfs = sorted(find_orfs(rec.seq, trans_table, min_len), 
+    out_dir = os.path.abspath(os.path.dirname(out_file))
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
+    fh = open(out_file, 'wt')
+
+    for rec in SeqIO.parse("{}".format(infile), file_format):
+        orfs = sorted(find_orfs(rec.seq, trans_table, min_len),
                       key=lambda orf: orf['start'])
+        print('{} has {}'.format(rec.id, len(orfs)))
         for j, orf in enumerate(orfs):
-            print('{:3}: {:5} {:5}'.format(j + 1,
-                                           orf['start'] + 1,
-                                           orf['stop'] + 1))
+            fh.write('>ORF{}({}-{})\n{}\n'.format(j + 1,
+                                                  orf['start'] + 1,
+                                                  orf['stop'] + 1,
+                                                  orf['seq']))
+
+    print('Done, see output file "{}"'.format(out_file))
 
 # --------------------------------------------------
 if __name__ == '__main__':
---
+++ run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -u
+
+FILE=""
+
+function USAGE() {
+    printf "Usage:\n  %s -f FILE\n\n" "$(basename "$0")"
+
+    echo "Required arguments:"
+    echo " -f FILE"
+    echo
+    exit "${1:-0}"
+}
+
+[[ $# -eq 0 ]] && USAGE 1
+
+while getopts :f:h OPT; do
+    case $OPT in
+        f)
+            FILE="$OPTARG"
+            ;;
+        h)
+            USAGE
+            ;;
+        :)
+            echo "Error: Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+        \?)
+            echo "Error: Invalid option: -${OPTARG:-""}"
+            exit 1
+    esac
+done
+
+[[ -z "$FILE" ]] && USAGE
+    
+if [[ ! -e "$FILE" ]]; then
+    echo "-f \"$FILE\" is not a file."
+    exit 1
+fi
+
+ORFS="$(basename "$FILE").orfs"
+./orf_predictor.py -o "$ORFS" "$FILE"
+
+blastp -query "$ORFS" -subject blast_database/conserved.fa -outfmt 6 -evalue 0.001
